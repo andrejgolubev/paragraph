18cc2f8b3359ac672d1e65b5526adcbfa0755018
Andrey <andrejgolubev14@gmail.com>
2026-01-24 16:54:39 +0300

    improved censor alg v2 + tests for new white-listed words

--- backend/api/logger.py
+++ backend/api/logger.py
@@ -2,20 +2,19 @@ import logging
 from pathlib import Path
 from backend.core.config import settings
 
+
+LOG_FORMATTER = logging.Formatter(settings.logging.log_format)
+LEVEL = settings.logging.log_level_value
+
 log = logging.getLogger(__name__)
+log.setLevel(LEVEL)
+log.propagate = False  # не дублировать на родительский handler
 
-logging.basicConfig(
-    level=(LEVEL := settings.logging.log_level_value),
-    format=(FORMAT := settings.logging.log_format),
+file_handler = logging.FileHandler(
+    Path(__file__).parent.parent / "app.log",
+    encoding="utf-8"
 )
-
-
-log.setLevel(LEVEL) # обязательно
-
-log_path = Path(__file__).parent.parent / "app.log"
-file_handler = logging.FileHandler(log_path)
 file_handler.setLevel(LEVEL)
-file_handler.setFormatter(logging.Formatter(FORMAT))
-file_handler.encoding = 'utf-8'
+file_handler.setFormatter(LOG_FORMATTER)
 
 log.handlers = [file_handler]
--- backend/tests/test_censor.py
+++ backend/tests/test_censor.py
@@ -1,5 +1,5 @@
 import pytest
-from backend.api.auth.censor.censor import has_cursive_words, ALLOWED_WORDS_PATHS
+from backend.api.auth.censor.censor import TEST_WORDS_PATH, has_cursive_words, ALLOWED_WORDS_PATHS
 
 
 @pytest.mark.asyncio
@@ -11,3 +11,13 @@ async def test_has_cursive_words():
                 assert not await has_cursive_words(
                     word
                 ), f'"{word}" from "{path.name}" marked as cursive.'
+
+
+@pytest.mark.asyncio 
+async def test_new_words():
+    with open(path:=TEST_WORDS_PATH, 'r', encoding='utf-8') as file: 
+        for row in file:
+            word = row.strip()
+            assert not await has_cursive_words(
+                word
+            ), f'"{word}" from "{path.name}" marked as cursive.'
