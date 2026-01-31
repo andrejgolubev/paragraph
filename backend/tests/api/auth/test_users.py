8cb1bab7d81ff8733dd448958512acbb58fa9498
Andrey <andrejgolubev14@gmail.com>
2026-02-01 00:31:59 +0300

    improve auth tests; add remaining user tests

--- backend/tests/api/auth/test_users.py
+++ backend/tests/api/auth/test_users.py
@@ -3,27 +3,40 @@ from httpx import AsyncClient, ASGITransport
 from asgi_lifespan import LifespanManager  
 
 from sqlalchemy import select, delete
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from backend.api.db.database import AsyncSessionLocal
 from backend.api.main import app
-from backend.core.config import settings
 from backend.api.db.models import User
-from backend.api.db.schemas import UserRegistration, UserLogin
+from backend.api.db.schemas import UserRegistration, UserLogin, UserUpdate
 from backend.api.auth.users import hash_password
 
 
+base_url = 'http://test'
+
 @pytest.mark.asyncio(loop_scope="session")
-async def test_register():
-    # Подготовка данных — отдельная сессия
-    async with AsyncSessionLocal() as db:
-        await db.execute(delete(User))
-        await db.commit()
+@pytest.mark.parametrize(
+    "group, expected",
+    [
+        (None, 200),
+        ("543", 200),
+        ("5413", 200),
+        ("5413M", 200), # латинская 
+        ("5413М", 200), # кириллическая
+        ("5423", 200),
+        ("164343", 404),
+        (" ", 404),
+    ]
+)
+async def test_register(group: str, expected, db: AsyncSession):
+    # Предварительная очистка БД
+    await db.execute(delete(User))
+    await db.commit()
     
-    payload = UserRegistration(
+    register_payload = UserRegistration(
         username="Иванов Иван",
         email="ivan@example.com",
         password="ComplexPass123!",
-        group_number=None,
+        group_number=group,
         accept_pd=True,
         accept_terms=True,
     ).model_dump()
@@ -31,59 +44,138 @@ async def test_register():
     async with LifespanManager(app):
         async with AsyncClient(
             transport=ASGITransport(app=app), 
-            base_url=settings.url.https
+            base_url=base_url
         ) as client:
-            response = await client.post("/user/register", json=payload)
+            response = await client.post("/user/register", json=register_payload)
 
-    assert response.status_code == 200
-    body = response.json()
-    assert body["status"] == "ok"
-    assert body["email"] == payload["email"]
-
-    # Проверка в БД — отдельная сессия
-    async with AsyncSessionLocal() as db:
+    assert (status_code:=response.status_code) == expected
+    
+    if status_code == 200:
         user_result = await db.scalars(
-            select(User).where(User.email == payload["email"])
+            select(User).where(User.email == register_payload["email"])
         )
         assert (user := user_result.first()) is not None
         assert user.role == "student"
 
 
 @pytest.mark.asyncio(loop_scope="session")
-async def test_login():
-    test_password = "ComplexPass123!"
+async def test_login(db: AsyncSession):
+    # Предварительная очистка БД
+    await db.execute(delete(User))
+    await db.commit()
     
-    # Подготовка данных — отдельная сессия
-    async with AsyncSessionLocal() as db:
-        await db.execute(delete(User))
-        await db.commit()
-        
-        user = User(
-            name="Тест Юзер",
-            email="login@example.com",
-            password=hash_password(test_password),
-            role="student",
-            active=True,
-            group_id=None,
-        )
-        db.add(user)
-        await db.commit()
-        user_email = user.email
+    user = User(
+        name="Иванов Иван",
+        email="ivan@example.com",
+        password=hash_password(test_password:='ComplexPass123!'),
+        role="student",
+        active=True,
+        group_id=None,
+    )
+    db.add(user)
+    await db.commit()
 
     login_payload = UserLogin(
-        email=user_email,
+        email=user.email,
         password=test_password,
     ).model_dump()
 
     async with LifespanManager(app):
         async with AsyncClient(
             transport=ASGITransport(app=app),
-            base_url=settings.url.https,
+            base_url=base_url,
         ) as client:
             response = await client.post("/user/login", json=login_payload)
 
     assert response.status_code == 200
-    body = response.json()
-    assert body["status"] == "ok"
     assert "access_token" in response.cookies
     assert "refresh_token" in response.cookies
+    
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_logout(db: AsyncSession):
+    # Предварительная очистка БД
+    await db.execute(delete(User))
+    await db.commit()
+    
+    user = User(
+        name="Иванов Иван",
+        email="ivan@example.com",
+        password=hash_password(test_password:='ComplexPass123!'),
+        role="student",
+        active=True,
+        group_id=None,
+    )
+    db.add(user)
+    await db.commit()
+
+    login_payload = UserLogin(
+        email=user.email,
+        password=test_password,
+    ).model_dump()
+
+    async with LifespanManager(app):
+        async with AsyncClient(
+            transport=ASGITransport(app=app),
+            base_url=base_url,
+        ) as client:
+            login_response = await client.post("/user/login", json=login_payload)
+
+            assert login_response.status_code == 200 
+            assert "access_token" in login_response.cookies
+            assert "refresh_token" in login_response.cookies
+            
+            logout_response = await client.post('/user/logout')
+
+
+    assert logout_response.status_code == 200
+    assert "access_token" not in logout_response.cookies
+    assert "refresh_token" not in logout_response.cookies
+    
+
+@pytest.mark.asyncio(loop_scope="session")
+@pytest.mark.parametrize("new_group, expected", [
+    (None, 200),
+    ("543", 200),
+    ("5413", 200),
+    ("5423", 200),
+    ("164343", 404),
+    (" ", 404),
+])
+async def test_update_profile(new_group: str, expected, db: AsyncSession): 
+    # Предварительная очистка БД
+    await db.execute(delete(User))
+    await db.commit()
+    
+    user = User(
+        name="Иванов Иван",
+        email="ivan@example.com",
+        password=hash_password(test_password:='ComplexPass123!'),
+        role="student",
+        active=True,
+        group_id=None,
+    )
+    db.add(user)
+    await db.commit()
+
+    update_payload = UserUpdate(
+        username=(new_username:='Сергеев Сергей'),
+        email=user.email,
+        group_number=new_group,
+        password=test_password,
+    ).model_dump()
+
+    async with LifespanManager(app):
+        async with AsyncClient(
+            transport=ASGITransport(app=app),
+            base_url=base_url,
+        ) as client:
+            response = await client.patch("/user/update-profile", json=update_payload)
+
+    body = response.json()
+    if expected == 200:
+        assert body["username"] == new_username
+        assert body["email"] == user.email
+        assert body["role"] == user.role
+        assert body["group"] == new_group
+    
