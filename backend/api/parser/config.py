84f0eac3b7f318e9881512c44953c49bcb097788
Andrey <andrejgolubev14@gmail.com>
2026-02-04 18:10:59 +0300

    optimize backend for parsing

--- backend/api/parser/config.py
+++ backend/api/parser/config.py
@@ -14,13 +14,36 @@ def create_driver():
     """Создаём WebDriver с оптимальными настройками для сервера"""
 
     options = Options()
-    options.add_argument('--headless') # Headless режим (без GUI)
+    options.add_argument('--headless=new') # Headless режим (без GUI)
     options.add_argument('--no-gpu') # Откл. GPU (для серверов без видеокарты)
     options.add_argument('--no-sandbox') # Откл. песочницу (нужно для запуска от root)
     options.add_argument('--disable-dev-shm-usage') # Откл. /dev/shm (решает проблемы с памятью)
     options.add_argument('--disable-notifications') # Откл. уведомления
     options.add_argument('--disable-extensions')
     options.add_argument('--disable-browser-side-navigation')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-software-rasterizer')
+
+    options.add_argument('--disable-background-networking')
+    options.add_argument('--disable-background-timer-throttling')
+    options.add_argument('--disable-backgrounding-occluded-windows')
+    options.add_argument('--disable-breakpad')
+    options.add_argument('--disable-component-extensions-with-background-pages')
+    options.add_argument('--disable-features=Translate,BackForwardCache')
+    options.add_argument('--disable-ipc-flooding-protection')
+    options.add_argument('--disable-renderer-backgrounding')
+
+    # Настройки для экономии памяти:
+    prefs = {
+        'profile.default_content_setting_values': {
+            'images': 2,  # Блокируем картинки
+            'javascript': 1,  # JS нужен
+        },
+        'credentials_enable_service': False,
+        'profile.password_manager_enabled': False
+    }
+    options.add_experimental_option('prefs', prefs)
+
 
     if os.path.exists(CHROME_BINARY) and os.path.exists(CHROMEDRIVER_BINARY):
         options.binary_location = CHROME_BINARY
