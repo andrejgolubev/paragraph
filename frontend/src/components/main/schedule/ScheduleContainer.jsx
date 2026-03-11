e2548ffb40aad3c573be9ee4d4c7495edbad8898
dvaJf <dvakgj@gmail.com>
2026-03-11 23:52:17 +0300

    auto choice data in mobile

--- frontend/src/components/main/schedule/ScheduleContainer.jsx
+++ frontend/src/components/main/schedule/ScheduleContainer.jsx
@@ -25,7 +25,7 @@ const ScheduleContainer = () => {
   const [homeworkAuthor, setHomeworkAuthor] = useState('')
   const [year, setYear] = useState(new Date().getFullYear())
   const [currentLessonInfo, setCurrentLessonInfo] = useState({})
-  const [mobileLesson, setMobileLesson] = useState(0)
+  const [mobileLesson, setMobileLesson] = useState((new Date().getDay()-1)%6)
   const windowSize = useWindowSize()
   const [debouncedWidth, setDebouncedWidth] = useState(windowSize.width)
   const widthDebounceRef = useRef(null)
