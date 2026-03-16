18b78f5e833821118929ee0c300ab628dd6055f1
Andrey <andrejgolubev14@gmail.com>
2026-03-17 00:24:45 +0300

    recolor header & add notes icon for desktop

--- frontend/src/components/main/MainContent.jsx
+++ frontend/src/components/main/MainContent.jsx
@@ -1,19 +1,25 @@
-import ScheduleContainer from "./schedule/ScheduleContainer" 
+import ScheduleContainer from "./schedule/ScheduleContainer"
 import Dropdown from "./Dropdown"
 import Tip from "./Tip"
-import { useEffect } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useWindowSize } from "../../hooks/useWindowSize"
 import { useDropdownStore } from "../../store/dropdownStore"
 import { useThemeStore } from "../../store/themeStore"
 import { useUiStore } from "../../store/uiStore"
-
+import notesToggle from "../../images/toggles/notes.svg"
+import notesToggleDark from "../../images/toggles/notes-dark.svg"
+import notesToggleActive from "../../images/toggles/notes_active.svg"
+import notesToggleDarkActive from "../../images/toggles/notes-dark_active.svg"
 
 const MainContent = () => {
-  const { groupDataValue, dateDataValue, } = useDropdownStore()
+  const { groupDataValue, dateDataValue } = useDropdownStore()
   const { darkTheme } = useThemeStore()
   const { tipActive, setTipActive } = useUiStore()
-    
-  useEffect( () => {
+  const [notesHovered, setNotesHovered] = useState(false)
+
+
+
+  useEffect(() => {
     if (groupDataValue) {
       setTipActive(false)
     } else {
@@ -21,14 +27,14 @@ const MainContent = () => {
     }
   }, [groupDataValue, setTipActive])
 
-
-  const {width} = useWindowSize() 
+  const { width } = useWindowSize()
   const isMobile = width < 1001
 
-
   return (
-    <div className='main-content'>
-      <div className={`schedule-wrap ${isMobile ? "mobile" : ""} ${darkTheme ? "dark" : ""}`}>
+    <div className="main-content">
+      <div
+        className={`schedule-wrap ${isMobile ? "mobile" : ""} ${darkTheme ? "dark" : ""}`}
+      >
         <div className="options">
           <Dropdown
             name="group"
@@ -46,16 +52,30 @@ const MainContent = () => {
             <p>C - Центральный корпус⠀⠀⠀⠀B - Бизнес-инкубатор</p>
             <p>L - Лабораторный корпус ⠀⠀⠀F - Первый корпус</p>
           </div>
+          <div className="notes-toggle">
+            <img
+              onMouseEnter={() => setNotesHovered(true)}
+              onMouseLeave={() => setNotesHovered(false)}
+              src={
+                darkTheme
+                  ? notesHovered
+                    ? notesToggleDarkActive
+                    : notesToggleDark
+                  : notesHovered
+                    ? notesToggleActive
+                    : notesToggle
+              }
+              alt="Заметки"
+            />
+          </div>
         </div>
-        {!groupDataValue && (
-          <Tip active={tipActive} />
-        )}
+        {!groupDataValue && <Tip active={tipActive} />}
         {groupDataValue && (
           <ScheduleContainer
             groupDataValue={groupDataValue}
             dateDataValue={dateDataValue}
-            />
-          )}
+          />
+        )}
       </div>
     </div>
   )
